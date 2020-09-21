5d3bd1f4735a2aaa8ae0124a072d278386ff1401
Pongsavet Phukaet <Pongsavet_name@hotmail.com>
2020-09-21 15:19:29 +0700

    let start

---
+++ js/app.js
@@ -0,0 +1,18 @@
+document.addEventListener('init', function (event) {
+    var page = event.target;
+
+    if (page.id === 'page1') {
+        page.querySelector('#move1').onclick = function () {
+            document.querySelector('#myNavigator').pushPage('views/john_wick_1.html', { data: { title: 'John Wick 1' } });
+        };
+        page.querySelector('#move2').onclick = function () {
+            document.querySelector('#myNavigator').pushPage('views/john_wick_2.html', { data: { title: 'John Wick 1' } });
+        };
+        page.querySelector('#move3').onclick = function () {
+            document.querySelector('#myNavigator').pushPage('views/john_wick_3.html', { data: { title: 'John Wick 1' } });
+        };
+        
+    } else if (page.id === 'page2') {
+        page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+    }
+});
