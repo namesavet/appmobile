fbd4a6a7565d5b49333fba81fe09f7cd24f02512
Pongsavet Phukaet <Pongsavet_name@hotmail.com>
2020-09-21 22:48:05 +0700

    insert pic move each page

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+
+
 document.addEventListener('init', function (event) {
     var page = event.target;
 
@@ -6,13 +8,14 @@ document.addEventListener('init', function (event) {
             document.querySelector('#myNavigator').pushPage('views/aladiin.html', { data: { title: 'Aladdin' } });
         };
         page.querySelector('#move2').onclick = function () {
-            document.querySelector('#myNavigator').pushPage('views/john_wick_2.html', { data: { title: 'Black Panther' } });
+            document.querySelector('#myNavigator').pushPage('views/blackpanther.html', { data: { title: 'Black Panther' } });
         };
         page.querySelector('#move3').onclick = function () {
-            document.querySelector('#myNavigator').pushPage('views/john_wick_3.html', { data: { title: 'Avengers'' } });
+            document.querySelector('#myNavigator').pushPage('views/avengers.html', { data: { title: 'Avengers' } });
         };
         
     } else if (page.id === 'page2') {
         page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
     }
 });
+
