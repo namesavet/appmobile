b832dda19c3ddd09313d74a8f8a61a7a540734c0
Pongsavet Phukaet <Pongsavet_name@hotmail.com>
2020-09-21 22:04:59 +0700

    insert movie page

--- js/app.js
+++ js/app.js
@@ -3,13 +3,13 @@ document.addEventListener('init', function (event) {
 
     if (page.id === 'page1') {
         page.querySelector('#move1').onclick = function () {
-            document.querySelector('#myNavigator').pushPage('views/john_wick_1.html', { data: { title: 'John Wick 1' } });
+            document.querySelector('#myNavigator').pushPage('views/aladiin.html', { data: { title: 'Aladdin' } });
         };
         page.querySelector('#move2').onclick = function () {
-            document.querySelector('#myNavigator').pushPage('views/john_wick_2.html', { data: { title: 'John Wick 1' } });
+            document.querySelector('#myNavigator').pushPage('views/john_wick_2.html', { data: { title: 'Black Panther' } });
         };
         page.querySelector('#move3').onclick = function () {
-            document.querySelector('#myNavigator').pushPage('views/john_wick_3.html', { data: { title: 'John Wick 1' } });
+            document.querySelector('#myNavigator').pushPage('views/john_wick_3.html', { data: { title: 'Avengers'' } });
         };
         
     } else if (page.id === 'page2') {
