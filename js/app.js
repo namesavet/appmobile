775487aaf8b4cffef53ea7425ab9cfd0e1ff6686
Pongsavet Phukaet <Pongsavet_name@hotmail.com>
2020-09-22 19:05:55 +0700

    link favorite movie

--- js/app.js
+++ js/app.js
@@ -1,33 +1,39 @@
-
-
-document.addEventListener('init', function (event) {
+document.addEventListener('init', function(event) {
     var page = event.target;
 
     if (page.id === 'page1') {
-        page.querySelector('#move1').onclick = function () {
+        page.querySelector('#move1').onclick = function() {
             document.querySelector('#myNavigator').pushPage('views/aladiin.html', { data: { title: 'Aladdin' } });
         };
-        page.querySelector('#move2').onclick = function () {
+        page.querySelector('#move2').onclick = function() {
             document.querySelector('#myNavigator').pushPage('views/blackpanther.html', { data: { title: 'Black Panther' } });
         };
-        page.querySelector('#move3').onclick = function () {
+        page.querySelector('#move3').onclick = function() {
             document.querySelector('#myNavigator').pushPage('views/avengers.html', { data: { title: 'Avengers' } });
         };
-        
+
     } else if (page.id === 'page2') {
         page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
-    } else if (page.id === 'search'){
-        page.querySelector('#search_aladdin').onclick = function () {
+    } else if (page.id === 'search') {
+        page.querySelector('#search_aladdin').onclick = function() {
             document.querySelector('#Navigator_search').pushPage('views/aladiin.html', { data: { title: 'Aladdin' } });
         };
-        page.querySelector('#search_black').onclick = function () {
+        page.querySelector('#search_black').onclick = function() {
             document.querySelector('#Navigator_search').pushPage('views/blackpanther.html', { data: { title: 'Black Panther' } });
         };
-        page.querySelector('#search_avengers').onclick = function () {
+        page.querySelector('#search_avengers').onclick = function() {
             document.querySelector('#Navigator_search').pushPage('views/avengers.html', { data: { title: 'Avengers' } });
+        };
+    } else if (page.id === 'favorite') {
+        page.querySelector('#favorite_aladdin').onclick = function() {
+            document.querySelector('#Navigator_favorite').pushPage('views/aladiin.html', { data: { title: 'Aladdin' } });
+        };
+        page.querySelector('#favorite_black').onclick = function() {
+            document.querySelector('#Navigator_favorite').pushPage('views/blackpanther.html', { data: { title: 'Black Panther' } });
+        };
+        page.querySelector('#favorite_avengers').onclick = function() {
+            document.querySelector('#Navigator_favorite').pushPage('views/avengers.html', { data: { title: 'Avengers' } });
         };
     }
 
 });
-
-
